2da0791868126096abf64bc3d891bf03adb47c25
Nicolas MASSART <nicolas.massart@consensys.net>
2019-02-25 14:32:13 +0100

    Added CLI ref doc

--- pantheon/src/test/java/tech/pegasys/pantheon/cli/RLPSubCommandTest.java
+++ pantheon/src/test/java/tech/pegasys/pantheon/cli/RLPSubCommandTest.java
@@ -116,8 +116,6 @@ public class RLPSubCommandTest extends CommandTestAbstract {
     final File file = File.createTempFile("ibftExtraData", "rlp");
 
     String jsonInput =
-        //        "[\"be068f726a13c8d46c44be6ce9d275600e1735a4\",\n"
-        //            + "\"5ff6f4b66a46a2b2310a6f3a93aaddc0d9a1c193\" ]";
         "{\"validators\":[\"be068f726a13c8d46c44be6ce9d275600e1735a4\",\n"
             + "\"5ff6f4b66a46a2b2310a6f3a93aaddc0d9a1c193\" ]}";
 
